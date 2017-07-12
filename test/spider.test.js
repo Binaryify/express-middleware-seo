fd2b52712a2b3f069a23fe0005291c5d901acbee
zhuangtongfa <zhuangtongfa@qq.com>
2017-07-12 10:48:59 +0800

    add unit test

---
+++ test/spider.test.js
@@ -0,0 +1,49 @@
+const assert = require('assert')
+const axios = require('axios')
+const cheerio = require('cheerio')
+describe('Simulation of crawler', () => {
+  it('Simulation of Google, response HTML should include "Im ready now"', done => {
+    axios
+      .get('https://chrome-headless-wjwcvoordw.now.sh/', {
+        headers: { 'User-Agent': 'googlebot' }
+      })
+      .then(function(response) {
+        const $ = cheerio.load(response.data)
+        console.log()
+        assert($('h1').text() == 'Im ready now')
+        done()
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  })
+  it('Simulation of Baidu, response HTML should include "Im ready now"', done => {
+    axios
+      .get('https://chrome-headless-wjwcvoordw.now.sh/', {
+        headers: { 'User-Agent': 'baiduspider' }
+      })
+      .then(function(response) {
+        const $ = cheerio.load(response.data)
+        console.log()
+        assert($('h1').text() == 'Im ready now')
+        done()
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  })
+  it('Response HTML should include "You are not a crawler, I will be ready after 1S"', done => {
+    axios
+      .get('https://chrome-headless-wjwcvoordw.now.sh/')
+      .then(function(response) {
+        const $ = cheerio.load(response.data)
+        assert(
+          $('h1').text() == 'You are not a crawler, I will be ready after 1S'
+        )
+        done()
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  })
+})
